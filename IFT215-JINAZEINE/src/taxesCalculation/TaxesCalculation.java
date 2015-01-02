af6d462d4a38395e5144b6f2cfe351e78bea1472
jinazeine <jinazeine@gmail.com>
2015-01-03 00:21:06 +0200

    taxed amount calculation

--- IFT215-JINAZEINE/src/taxesCalculation/TaxesCalculation.java
+++ IFT215-JINAZEINE/src/taxesCalculation/TaxesCalculation.java
@@ -1,15 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package taxesCalculation;
 
-/**
- *
- * @author jina
- */
 public class TaxesCalculation {
+    private int yearlySalary;
+    private boolean married;
+    private int nbKids;
+   
+
+public TaxesCalculation (int yearlySalary, boolean married, int nbKids){
+    this.yearlySalary=yearlySalary;
+    this.married=married;
+    this.nbKids=nbKids;
     
 }
+public int getYearlySalary(){
+    return yearlySalary;
+}
+public void setYearlySalary(int yearlySalary){
+   this.yearlySalary=yearlySalary;
+}
+
+public boolean hasMarried(){
+    return married;
+}
+public void setMarried(boolean married){
+    this.married=married;
+}
+public int getNbKids(){
+    return nbKids;
+}
+public void setNbKids(int nbKids){
+    this.nbKids=nbKids;
+}
+
+public int getTaxesAmount(){
+    int taxesAmount;
+    int abatement;
+    
+    if (married==true){
+        abatement=7500000+2500000+nbKids*500000;
+    } else{
+        abatement=7500000;
+    }
+    taxesAmount=yearly
+        
+            
+       
+        
+    
+    
+  return taxesAmount;
+}
+}
+
+
