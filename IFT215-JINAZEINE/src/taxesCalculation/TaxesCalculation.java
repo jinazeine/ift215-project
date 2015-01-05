5517985b16f034e32d52350b203f4e8a0a5d1a3b
jinazeine <jinazeine@gmail.com>
2015-01-05 12:34:25 +0200

    calculating taxes Amount

--- IFT215-JINAZEINE/src/taxesCalculation/TaxesCalculation.java
+++ IFT215-JINAZEINE/src/taxesCalculation/TaxesCalculation.java
@@ -38,7 +38,7 @@ public int getTaxesAmount(){
     int taxedAmount;
     int abatement;
     int newTaxedAmount;
-    int t1,t2,t3,t4,t5,t6,t7,t8;
+    int t1,t2,t3,t4,t5,t6;
     if (married==true){
         abatement=7500000+2500000+nbKids*500000;
     } else{
@@ -51,26 +51,64 @@ public int getTaxesAmount(){
         t1=6000000*(2/100);
         newTaxedAmount=taxedAmount-6000000;
         t2=9000000*(4/100);
-        newTaxedAmount=taxedAmount-9000000;
+        newTaxedAmount=newTaxedAmount-9000000;
         t3=15000000*(7/100);
-        newTaxedAmount=taxedAmount-15000000;
+        newTaxedAmount=newTaxedAmount-15000000;
         t4=30000000*(11/100);
-        newTaxedAmount=taxedAmount-30000000;
+        newTaxedAmount=newTaxedAmount-30000000;
         t5=60000000*(15/100);
-        newTaxedAmount=taxedAmount-60000000;
+        newTaxedAmount=newTaxedAmount-60000000;
         t6=newTaxedAmount*(20/100);
+        taxesAmount=(t1+t2+t3+t4+t5+t6)/12;
         
-   
-   }
-   
-   
-      
-        
-            
        
+   } 
+    else { 
+        if((taxedAmount<120000000)&&(taxedAmount>60000000)){
+           t1=6000000*(2/100);
+           newTaxedAmount=taxedAmount-6000000;
+           t2=9000000*(4/100);
+           newTaxedAmount=newTaxedAmount-9000000;
+           t3=15000000*(7/100);
+           newTaxedAmount=newTaxedAmount-15000000;
+           t4=30000000*(11/100);
+           newTaxedAmount=newTaxedAmount-30000000;
+           t5=newTaxedAmount*(15/100);
+           taxesAmount=(t1+t2+t3+t4+t5)/12;
+        } else{
+            if((taxedAmount<60000000)&&(taxedAmount>30000000)){
+                 t1=6000000*(2/100);
+                 newTaxedAmount=taxedAmount-6000000;
+                 t2=9000000*(4/100);
+                 newTaxedAmount=newTaxedAmount-9000000;
+                 t3=15000000*(7/100);
+                 newTaxedAmount=newTaxedAmount-15000000;
+                 t4=newTaxedAmount*(11/100);
+                 taxesAmount=(t1+t2+t3+t4)/12;
+            } else{
+                if((taxedAmount<30000000)&&(taxedAmount>15000000)){
+                       t1=6000000*(2/100);
+                       newTaxedAmount=taxedAmount-6000000;
+                       t2=9000000*(4/100);
+                       newTaxedAmount=newTaxedAmount-9000000;
+                       t3=newTaxedAmount*(7/100);
+                       taxesAmount=(t1+t2+t3)/12;
+                } else {
+                    if ((taxedAmount<15000000)&&(taxedAmount>6000000)){
+                            t1=6000000*(2/100);
+                            newTaxedAmount=taxedAmount-6000000;
+                            t2=newTaxedAmount*(4/100);
+                            taxesAmount=(t1+t2)/12;
+                    } else {
+                      t1=taxedAmount*(2/100);
+                      taxesAmount=t1/12;
+                    }
+                }  
+            }
+        }
         
-    
-    
+    }
+  
   return taxesAmount;
 }
 }
